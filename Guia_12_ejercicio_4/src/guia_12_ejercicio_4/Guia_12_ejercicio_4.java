1610537015b515cd90a2a0fee257714688b4d84f
brunodev12 <brunocarthery12@gmail.com>
2023-05-30 16:36:41 -0300

    ejercicio 4 guia 12

---
+++ Guia_12_ejercicio_4/src/guia_12_ejercicio_4/Guia_12_ejercicio_4.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia_12_ejercicio_4;
+
+/**
+ *
+ * @author bruno
+ */
+public class Guia_12_ejercicio_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
